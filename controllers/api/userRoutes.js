788ad4f9e67b19bfd63c6423070d37de0fff2db4
wayne80361 <wayne80361@gmail.com>
2023-10-26 21:58:53 -0400

    Get All Users route for testing, may take it down later

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const userData = await User.findAll();
+
+    // Serialize data so the template can read it
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Route for creating a new user
 // /api/users
 router.post("/", async (req, res) => {
