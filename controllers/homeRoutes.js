eeb0142e6ac2ff7c6a9a3a882f0d269e1983e981
wayne80361 <wayne80361@gmail.com>
2023-10-26 21:42:38 -0400

    add route front-end route for render dashboard and dashboaard.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,4 +51,24 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// Use withAuth middleware to prevent access to route
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
